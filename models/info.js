6ef4ab69d47a01f20ff4a9fb856b28878706cb99
Deepika <kulkdeepika@gmail.com>
2020-03-19 13:22:05 -0700

    added table creation code to existing passport code base

---
+++ models/info.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+    var Info = sequelize.define("Info", {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      interest: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      aboutMe: {
+        type: DataTypes.TEXT
+      },
+      available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      }
+    });
+
+    Info.associate = function(models){
+      Info.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        },
+        onDelete: "cascade"
+      });
+    }
+    return Info;
+}
