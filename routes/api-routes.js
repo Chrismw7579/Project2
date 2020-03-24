09f74410c77feb9c7c8eb3aeae1bdec43dc13de7
chrismw <chrismw7579@gmail.com>
2020-03-23 20:40:18 -0700

    sortByInterest filters out members who are not available

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -106,7 +106,7 @@ module.exports = function(app) {
 		let OthersInterests = [];
 
 		for (let i = 0; i < data.length; i++) {
-			if (id != data[i].dataValues.id) { // excludes the user from the list
+			if (id != data[i].dataValues.id && data[i].dataValues.available == 1) { // excludes the user from the list
 				
 				const obj = {
 					id: data[i].dataValues.id,
