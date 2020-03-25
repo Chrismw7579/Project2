3b0bd1811a9f2d05a58a86536cbe28ef59937c6b
Megha Murthy <m.megha21@gmail.com>
2020-03-24 20:12:55 -0700

    added new code to members

--- public/js/members.js
+++ public/js/members.js
@@ -8,32 +8,50 @@ $(document).ready(function() {
 		$(".member-name").text(data.email);
 	});
 });
+
 $('#availabilitySwitch').on("click",function(){
-	console.log(document.getElementById('availabilitySwitch').checked);
-
-	if($('#availabilitySwitch').checked==true){
-		console.log("I am being clicked");
-		var newStatus={
-			available:1
-		};
-		updateToggle(newStatus.available); 
-
+	let newAvailability = {
+	  available : document.getElementById('availabilitySwitch').checked ? 1 : 0
 	}
-	else{
-		//console.log("I am being clicked");
- 	var newStatus={
-		 available:0
-		};
-		updateToggle(newStatus.available);
-  
-	}
-   
-});
-
-function updateToggle(available){
-	$.get('/api/toggle',{
-		available:available
-	}).then(function(data){
-		console.log("status sent to api-routes is:",data);
+	console.log(newAvailability);
+	$.ajax({
+	  method: "PUT",
+	  url: "/api/availability",
+	  data: newAvailability
+	})
+	  .then(function(data) {
+		console.log(data)
 	});
-}
+});
+// $('#availabilitySwitch').on("click",function(){
+// 	console.log(document.getElementById('availabilitySwitch').checked);
+// 	console.log("start");
+// 	console.log($('#availabilitySwitch').checked);
+// 	console.log("end");
+
+// 	if($('#availabilitySwitch').checked==true){
+// 		console.log("I am being clicked");
+// 		var newStatus={
+// 			available:1
+// 		};
+// 		updateToggle(newStatus.available); 
+
+// 	}
+// 	else{
+// 		//console.log("I am being clicked");
+//  	var newStatus={
+// 		 available:0
+// 		};
+// 		updateToggle(newStatus.available);
+  
+// 	}
+   
+// });
+
+// function updateToggle(available){
+// 	$.get('/api/toggle',{
+// 		available:available
+// 	}).then(function(data){
+// 		console.log("status sent to api-routes is:",data);
+// 	});
+// }
