4f2d4f88497577189a9d8bfaa09d53ca7ddc8a3a
Megha Murthy <m.megha21@gmail.com>
2020-03-25 07:18:35 -0700

    update route changes

--- public/js/members.js
+++ public/js/members.js
@@ -9,53 +9,53 @@ $(document).ready(function() {
 	});
 });
 
-//$('#availabilitySwitch').on("click",function(){
-	
-// 	let newAvailability = {
-// 	  available : document.getElementById('availabilitySwitch').checked ? 1 : 0
-// 	}
-// 	console.log(newAvailability);
-// 	$.ajax({
-// 	  method: "PUT",
-// 	  url: "/api/availability",
-// 	  data: newAvailability
-// 	})
-// 	  .then(function(data) {
-// 		console.log(data)
-// 	});
-// });
-
 $('#availabilitySwitch').on("click",function(){
-	console.log(document.getElementById('availabilitySwitch').checked);
 	
-	// console.log(document.getElementById('availabilitySwitch').checked);
-	// console.log("start");
-	// console.log($('#availabilitySwitch').checked);
-	// console.log("end");
-
-	if(document.getElementById('availabilitySwitch').checked===true){
-		console.log("I am being clicked");
-		var newStatus={
-			available:1
-		};
-		updateToggle(newStatus.available); 
-
+	let newAvailability = {
+	  available : document.getElementById('availabilitySwitch').checked ? 1 : 0
 	}
-	else{
-		//console.log("I am being clicked");
- 	var newStatus={
-		 available:0
-		};
-		updateToggle(newStatus.available);
-  
-	}
-   
+	console.log(newAvailability);
+	$.ajax({
+	  method: "PUT",
+	  url: "/api/availability",
+	  data: newAvailability
+	})
+	  .then(function(data) {
+		console.log(data)
+	});
 });
 
-function updateToggle(available){
-	$.get('/api/toggle',{
-		available:available
-	}).then(function(data){
-		console.log("status sent to api-routes is:",data);
-	});
-}
+// $('#availabilitySwitch').on("click",function(){
+// 	console.log(document.getElementById('availabilitySwitch').checked);
+	
+// 	console.log(document.getElementById('availabilitySwitch').checked);
+// 	console.log("start");
+// 	console.log($('#availabilitySwitch').checked);
+// 	console.log("end");
+
+// 	if(document.getElementById('availabilitySwitch').checked===true){
+// 		console.log("I am being clicked");
+// 		var newStatus={
+// 			available:1
+// 		};
+// 		updateToggle(newStatus.available); 
+
+// 	}
+// 	else{
+// 		//console.log("I am being clicked");
+//  	var newStatus={
+// 		 available:0
+// 		};
+// 		updateToggle(newStatus.available);
+  
+// 	}
+   
+// });
+
+// function updateToggle(available){
+// 	$.get('/api/toggle',{
+// 		available:available
+// 	}).then(function(data){
+// 		console.log("status sent to api-routes is:",data);
+// 	});
+// }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,89 +3,89 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
+	// Using the passport.authenticate middleware with our local strategy.
+	// If the user has valid login credentials, send them to the members page.
+	// Otherwise the user will be sent an error
+	app.post("/api/login", passport.authenticate("local"), function(req, res) {
+		res.json(req.user);
+	});
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    let username =  req.body.username;
-    let location = req.body.location;
-    let interest = req.body.interest;
-    let aboutMe = req.body.aboutMe;
-    let available = req.body.available;
+	// Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+	// how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+	// otherwise send back an error
+	app.post("/api/signup", function(req, res) {
+		let username =  req.body.username;
+		let location = req.body.location;
+		let interest = req.body.interest;
+		let aboutMe = req.body.aboutMe;
+		let available = req.body.available;
     
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(function(data) {
+		db.User.create({
+			email: req.body.email,
+			password: req.body.password
+		})
+			.then(function(data) {
         
-        console.log("HIT!!");
-        console.log(data.dataValues.id);
-        console.log("END!!")
-        console.log(username + " " + location + " " + interest + " " + aboutMe + " " + available)
-          db.Info.create({
-            username: username,
-            location: location,
-            interest: interest,
-            aboutMe:  aboutMe,
-            available: available,
-            UserId: data.dataValues.id
-          }).then(function(){
-              res.redirect(307, "/api/login");
-          }) .catch(function(err) {
-            console.log(err);
-            res.status(401).json(err);
-          });   
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
+				console.log("HIT!!");
+				console.log(data.dataValues.id);
+				console.log("END!!")
+				console.log(username + " " + location + " " + interest + " " + aboutMe + " " + available)
+				db.Info.create({
+					username: username,
+					location: location,
+					interest: interest,
+					aboutMe:  aboutMe,
+					available: available,
+					UserId: data.dataValues.id
+				}).then(function(){
+					res.redirect(307, "/api/login");
+				}) .catch(function(err) {
+					console.log(err);
+					res.status(401).json(err);
+				});   
+			})
+			.catch(function(err) {
+				res.status(401).json(err);
+			});
+	});
 
-  // Route for logging user out
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
+	// Route for logging user out
+	app.get("/logout", function(req, res) {
+		req.logout();
+		res.redirect("/");
+	});
 
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      db.Info.findOne({
-        where: {
-          UserId: req.user.id
-        }
-      }).then(function(results){
-        console.log(results);
-        res.json({
-          email: req.user.email,
-          id: req.user.id,
-          username: results.dataValues.username,
-          location: results.dataValues.location,
-          interest: results.dataValues.interest,
-          aboutMe: results.dataValues.aboutMe,
-          available: results.available
-        });
+	// Route for getting some data about our user to be used client side
+	app.get("/api/user_data", function(req, res) {
+		if (!req.user) {
+			// The user is not logged in, send back an empty object
+			res.json({});
+		} else {
+			// Otherwise send back the user's email and id
+			// Sending back a password, even a hashed password, isn't a good idea
+			db.Info.findOne({
+				where: {
+					UserId: req.user.id
+				}
+			}).then(function(results){
+				console.log(results);
+				res.json({
+					email: req.user.email,
+					id: req.user.id,
+					username: results.dataValues.username,
+					location: results.dataValues.location,
+					interest: results.dataValues.interest,
+					aboutMe: results.dataValues.aboutMe,
+					available: results.available
+				});
 
-      })
+			})
       
-    }
-  });
+		}
+	});
 
 
-  // Route for returning to the user other members with the most common interests
+	// Route for returning to the user other members with the most common interests
 	app.get("/api/others", (req, res) => {
 		// Finds the users location
 		let interests = '';
@@ -183,37 +183,37 @@ module.exports = function(app) {
 		return(list);
 	};
 
-//update the toggle switch value in db
+	//update the toggle switch value in db
 
-	// app.put("/api/availability", (req, res)=>{
-	// 	console.log("route Hit")
-	// 	db.Info.update(
-	// 		req.body,
-	// 		{
-	// 		where: {
-	// 			id: req.user.id
-	// 		}
-	// 		}).then(function(dbPost) {
-	// 		res.json(dbPost);
-	// 	});
-	// })
-	app.get('/api/toggle',function(req,res){
-		console.log(req.query.available);
-		db.Info.update({available:req.query.available},
+	app.put("/api/availability", (req, res)=>{
+		console.log("route Hit")
+		db.Info.update(
+			req.body,
 			{
-				where:{
-					UserId:req.user.id
-					//available:1
-					
+				where: {
+					id: req.user.id
 				}
-				
-			}).then(result=>{
-				res.json({
-					available:result[0]
-				})
-				//console.log("The obtained result is:",result[0]);
-			})
+			}).then(function(dbPost) {
+			res.json(dbPost);
+		});
 	});
+	// app.get('/api/toggle',function(req,res){
+	// 	console.log(req.query.available);
+	// 	db.Info.update({available:req.query.available},
+	// 		{
+	// 			where:{
+	// 				UserId:req.user.id
+	// 				//available:1
+					
+	// 			}
+				
+	// 		}).then(result=>{
+	// 			res.json({
+	// 				available:result[0]
+	// 			})
+	// 			//console.log("The obtained result is:",result[0]);
+	// 		})
+	// });
 
 
 
