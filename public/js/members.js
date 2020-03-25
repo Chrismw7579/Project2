fc8ec72374145f7b35c6a352474c7f0ba3893680
Deepika <kulkdeepika@gmail.com>
2020-03-25 13:10:48 -0700

    added more functionality to availability switch

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 const pageOne = $('#firstPage');
 const pageTwo = $('#secondPage');
 const pageThree = $('#thirdPage');
+
   $('#availabilitySwitch').on("click",function(){
     
       let newAvailability = {
@@ -16,7 +17,18 @@ const pageThree = $('#thirdPage');
         data: newAvailability
       })
         .then(function(data) {
-          console.log(data)
+          console.log(data);
+
+        if(!newAvailability.available){
+          pageTwo.css('display', 'none');
+          pageThree.css('display', 'initial');
+          pageOne.css('display', 'none');
+        }
+        else{
+          pageTwo.css('display', 'none');
+          pageThree.css('display', 'none');
+          pageOne.css('display', 'initial');
+        }
       });
   });
  
@@ -28,6 +40,16 @@ const pageThree = $('#thirdPage');
     console.log(userId);
     $(".member-name").text(data.username);
     document.getElementById('availabilitySwitch').checked = data.available;
+      if(data.available){
+        pageTwo.css('display', 'none');
+        pageThree.css('display', 'none');
+        pageOne.css('display', 'initial');
+      }
+      else{
+        pageTwo.css('display', 'none');
+        pageThree.css('display', 'initial');
+        pageOne.css('display', 'none');
+      }
   });
 
   //Hangout Button Event Listner
@@ -150,75 +172,3 @@ const pageThree = $('#thirdPage');
   }
 
 });//End of document.ready
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Code to generate cards; must be added in a event listener function
-
-  // for(let i=0; i<5; i++){
-  //   let icon1 = $("<i>");
-  //   icon1.attr("class", "material-icons right");
-  //   icon1.text("more_vert");
-
-  //   let sp1 = $("<span>");
-  //   sp1.attr("class", "card-title activator grey-text text-darken-4");
-  //   sp1.text("Mark Fischer");
-
-  //   let anchor = $("<a>");
-  //   anchor.attr("class", "waves-effect waves-light btn btn-small indigo lighten-2");
-  //   anchor.text("CONNECT");
-
-  //   let pTag1 = $("<p>");
-
-  //   let divCardContent = $("<div>");
-  //   divCardContent.attr("class", "card-content");
-
-  //   sp1.append(icon1);
-  //   pTag1.append(anchor);
-  //   divCardContent.append(sp1);
-  //   divCardContent.append(pTag1);
-
-
-  //   let icon2 = $("<i>");
-  //   icon2.attr("class", "material-icons right");
-  //   icon2.text("close");
-
-  //   let sp2 = $("<span>");
-  //   sp2.attr("class", "card-title grey-text text-darken-4")
-  //   sp2.text("Card Title");
-
-  //   let pTag2 = $("<p>");
-  //   pTag2.text("Here is some more information about this product that is only revealed once clicked on.");
-
-  //   let divCardReveal = $("<div>");
-  //   divCardReveal.attr("class", "card-reveal");
-
-  //   sp2.append(icon2);
-  //   divCardReveal.append(sp2);
-  //   divCardReveal.append(pTag2);
-
-  //   let divCard = $("<div>");
-  //   divCard.attr("class", "card-content");
-  //   divCard.append(divCardContent);
-  //   divCard.append(divCardReveal);
-
-  //   $("#cardsHolderDiv").append(divCard);
-
-  // }
